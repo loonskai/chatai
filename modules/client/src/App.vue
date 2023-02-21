5bcbeb3a58549ad6e1e7f05074b906a216c22496
Siarhei Lunski <loonskai@gmail.com>
2023-02-21 21:10:54 +0100

    Add cursor

--- modules/client/src/App.vue
+++ modules/client/src/App.vue
@@ -2,14 +2,14 @@
   <section class="relative flex bg-slate-100 m-auto w-120 px-10 pb-20 max-w-screen-sm h-screen">
     <ul class="flex flex-col p-4 pb-10 overflow-y-scroll w-full h-full items-start">
       <span v-if="messages.length === 0" class="text-slate-400 w-full text-center justify-self-center">Nothing here</span>
-      <li v-for="message in messages" :key="message.date" class="mb-4 bg-white rounded-md outline outline-1 outline-slate-100 py-2 px-5 shadow-md w-1/2" :class="{'self-start': message.author === 'me', 'self-end': message.author === 'ai'}">
+      <li v-for="message, idx in messages" :key="message.date" class="mb-4 bg-white rounded-md outline outline-1 outline-slate-100 py-2 px-5 shadow-md w-1/2" :class="{'self-start': message.author === 'me', 'self-end': message.author === 'ai'}">
         <div>
           <div class="flex align-middle justify-between">
             <span class="font-semibold">{{ message.author === 'me' ? 'Me' : 'Friend' }}</span>
             <span class="text-xs text-slate-500">{{ `${new Date(message.date).toLocaleTimeString()} ${new Date(message.date).toLocaleDateString()}` }}</span>
           </div>
           <div>
-            <span>{{message.text}}</span>
+            <span :class="{'cursor': loading && message.author === 'ai' && idx === messages.length - 1}">{{message.text}}</span>
           </div>
         </div>
       </li>
@@ -19,6 +19,7 @@
         class="w-96 rounded-md py-1 px-3 m-1 outline outline-1 outline-slate-300 focus:outline-emerald-500" 
         :value="requestMessage" 
         @input="update"
+        @keyup.enter="submit(requestMessage)"
       />
       <button 
         class="flex w-32 justify-center rounded-md py-1 px-3 m-1 bg-emerald-500 text-white disabled:opacity-50" 
@@ -121,4 +122,18 @@ onUnmounted(() => {
 
 <style>
 .crossIcon{width:1em;height:1em;vertical-align:middle;fill:currentColor;overflow:hidden;}.spinner_EUy1{animation:spinner_grm3 1.2s infinite}.spinner_f6oS{animation-delay:.1s}.spinner_g3nX{animation-delay:.2s}.spinner_nvEs{animation-delay:.3s}.spinner_MaNM{animation-delay:.4s}.spinner_4nle{animation-delay:.5s}.spinner_ZETM{animation-delay:.6s}.spinner_HXuO{animation-delay:.7s}.spinner_YaQo{animation-delay:.8s}.spinner_GOx1{animation-delay:.9s}.spinner_4vv9{animation-delay:1s}.spinner_NTs9{animation-delay:1.1s}.spinner_auJJ{transform-origin:center;animation:spinner_T3O6 6s linear infinite}@keyframes spinner_grm3{0%,50%{animation-timing-function:cubic-bezier(.27,.42,.37,.99);r:1px}25%{animation-timing-function:cubic-bezier(.53,0,.61,.73);r:2px}}@keyframes spinner_T3O6{0%{transform:rotate(360deg)}100%{transform:rotate(0deg)}}
+@keyframes cursor-blink {
+  0% {opacity: 0}
+	49%{opacity: 0}
+	50% {opacity: 1}
+}
+.cursor::after {
+  content: "";
+  width: 2px;
+  height: 15px;
+  margin-left: 2px;
+  background-color: #000;
+  display: inline-block;
+  animation: cursor-blink 1s infinite;
+}
 </style>
